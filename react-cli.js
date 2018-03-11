9eaa9c7cdf8d636216dcef6d5c2885028394e8b0
guojihang <guojihang@baidu.com>
2018-03-11 14:05:54 +0800

    修改部分已知BUG

--- react-cli.js
+++ react-cli.js
@@ -197,7 +197,7 @@ ReactDOM.render(<Root />, document.getElementById('root'));
 `
 {
     "name": "react-simple-cli",
-    "version": "1.0.0",
+    "version": "1.1.3",
     "description": "The most simple of react project",
     "main": "index.js",
     "scripts": {
@@ -205,8 +205,34 @@ ReactDOM.render(<Root />, document.getElementById('root'));
         "build": "node ./scripts/build.js",
         "init": "node ./scripts/init.js"
     },
-    "dependencies": {},
-    "devDependencies": {},
+    "dependencies": {
+        "antd": "^3.2.0",
+        "axios": "^0.17.1",
+        "mobx": "^3.5.1",
+        "mobx-react": "^4.4.2",
+        "moment": "^2.20.1",
+        "react": "^16.2.0",
+        "react-dom": "^16.2.0",
+        "react-router-dom": "^4.2.2"
+    },
+    "devDependencies": {
+        "babel": "^6.23.0",
+        "babel-core": "^6.26.0",
+        "babel-loader": "^7.1.2",
+        "babel-plugin-transform-decorators-legacy": "^1.3.4",
+        "babel-preset-latest": "^6.24.1",
+        "babel-preset-react": "^6.24.1",
+        "babel-preset-stage-0": "^6.24.1",
+        "css-loader": "^0.28.9",
+        "file-loader": "^1.1.6",
+        "html-webpack-plugin": "^2.30.1",
+        "less": "^2.7.3",
+        "less-loader": "^4.0.5",
+        "style-loader": "^0.20.1",
+        "url-loader": "^0.6.2",
+        "webpack": "^3.10.0",
+        "webpack-dev-server": "^2.11.1"
+    },
     "author": "JihangGuo",
     "license": "ISC"
 }
@@ -276,34 +302,42 @@ const prodModules = [
 ];
 
 // 由于spawn命令参数传递的特殊性 故在数组[0][-1]加入额外参数
-let commontProd = prodModules.map((item) => {
-  return item;
-});
-let commontDev = devModules.map((item) => {
-  return item;
-});
-commontProd.push('--save');
-commontProd.unshift('i');
-commontDev.push('--save-dev');
-commontDev.unshift('i');
+// let commontProd = prodModules.map((item) => {
+//   return item;
+// });
+// let commontDev = devModules.map((item) => {
+//   return item;
+// });
+// commontProd.push('--save');
+// commontProd.unshift('i');
+// commontDev.push('--save-dev');
+// commontDev.unshift('i');
 
 // 运行线程任务
-process.execSync(\`npm config set registry \${npmRegistry}\`);
-console.log("设置npm源为淘宝源成功");
+// process.execSync(\`npm config set registry \${npmRegistry}\`);
+// console.log("设置npm源为淘宝源成功");
 
-let runShellTwo = process.spawn('npm',commontProd);
-runShellTwo.stdout.on('data', (data) => {
-  console.log("正在下载依赖模块 : \\n" + data);
-});
-runShellTwo.on('close', (data) => {
-  console.log("依赖模块安装成功\\n");
-});
+// let runShellTwo = process.spawn('npm',commontProd);
+// runShellTwo.stdout.on('data', (data) => {
+//   console.log("正在下载依赖模块 : \\n" + data);
+// });
+// runShellTwo.on('close', (data) => {
+//   console.log("依赖模块安装成功\\n");
+// });
 
-let runShellThree = process.spawn('npm',commontDev);
-runShellThree.stdout.on('data', (data) => {
+// let runShellThree = process.spawn('npm',commontDev);
+// runShellThree.stdout.on('data', (data) => {
+//   console.log("正在下载开发依赖模块 : \\n" + data);
+// });
+// runShellThree.on('close', (data) => {
+//   console.log("开发依赖模块安装成功\\n项目初始化完成");
+// });
+
+let runShellAll = process.spawn('npm',['install']);
+runShellAll.stdout.on('data', (data) => {
   console.log("正在下载开发依赖模块 : \\n" + data);
 });
-runShellThree.on('close', (data) => {
+runShellAll.on('close', (data) => {
   console.log("开发依赖模块安装成功\\n项目初始化完成");
 });
 `;
@@ -363,10 +397,10 @@ const webpack = require('webpack');
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
     // 打包入口文件
-    entry: ['webpack-dev-server/client?http://localhost:8080/', process.cwd() + '/../src/index.js', process.cwd() + '/../node_modules/antd/dist/antd.css'],
+    entry: ['webpack-dev-server/client?http://localhost:8080/', __dirname + '/../src/index.js'],
     // 打包出口文件
     output: {
-        path: process.cwd() + '/../src/',
+        path: __dirname + '/../src/',
         filename: 'bundle.js'
     },
     // loader 进行非js文件的转换打包
@@ -397,8 +431,8 @@ module.exports = {
     plugins: [
         new webpack.HotModuleReplacementPlugin(),// 热模块替换插件
         new HtmlWebpackPlugin({
-            filename: process.cwd() + '/../src/index.html',
-            template: process.cwd() + '/../static/template.html',
+            filename: __dirname + '/../src/index.html',
+            template: __dirname + '/../static/template.html',
             inject: 'body',
             hash: true,
             minify: {
@@ -409,7 +443,7 @@ module.exports = {
     ],
     // webpack-dev-server配置
     devServer: {
-        contentBase: process.cwd() + '/../src/', // 只能精确到根文件夹 默认文件为index.html
+        contentBase: __dirname + '/../src/', // 只能精确到根文件夹 默认文件为index.html
         historyApiFallback: true,// 在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
         inline: true,// 设置为true，当源文件改变时会自动刷新页面
         port: 8080,// 设置默认监听端口，如果省略，默认为"8080"
@@ -435,10 +469,10 @@ const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 module.exports = {
     devtool: 'cheap-module-source-map',
     //打包入口文件
-    entry: [process.cwd() + '/../src/index.js', process.cwd() + '/../node_modules/antd/dist/antd.css'],
+    entry: [__dirname + '/../src/index.js'],
     //打包出口文件
     output: {
-        path: process.cwd() + '/../build/js/',
+        path: __dirname + '/../build/js/',
         filename: 'bundle.min.js',
     },
     //loader 进行非js文件的转换打包
@@ -469,7 +503,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             title:'react-app',
             filename: '../index.html',
-            template:process.cwd() + '/../static/template.html',
+            template:__dirname + '/../static/template.html',
             inject: 'body',
             hash: true,
             minify: {
